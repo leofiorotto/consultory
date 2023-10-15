eef0047fd44b40c1679493055f728a82438454f5
leofiorotto <leo.fiorotto@hotmail.com>
2023-10-14 22:23:44 -0300

    fix contact and footer

---
+++ src/components/translations.js
@@ -0,0 +1,18 @@
+export const translations = {
+    en: {
+        s1Tittle: "Experts in IT solutions",
+        s1p1: "We are an IT services company specialized in providing customized solutions.",
+        s1p2: "Always committed to quality and excellence.",
+        s1btn1: "Contact us",
+        s1btn2: "Know more about us",
+      // Agrega más traducciones según sea necesario
+    },
+    es: {
+      s1Tittle: "Expertos en soluciones IT",
+      s1p1: "Somos una empresa de servicios IT especializada en brindar soluciones a la medida.",
+      s1p2: "Siempre comprometidos con la calidad y la excelencia.",
+      s1btn1: "Contactanos",
+      s1btn2: "Conoce mas de nosotros",
+      // Agrega más traducciones según sea necesario
+    },
+  };
